8e4aa0353d17b35a069696742c0c6f3d9c639025
v0rvex <105231617+v0rvex@users.noreply.github.com>
2024-02-29 21:45:15 +0500

    Add files via upload

---
+++ computers/scripts/slider.js
@@ -0,0 +1,13 @@
+const radioButtons = document.querySelectorAll('input[type=radio]');
+
+
+const slideShow = () => {
+ for(let i =0; i<radioButtons.length; i++){
+   setInterval(() => {
+     console.log(i)
+   }, 3000)
+ }
+}
+
+
+slideShow();
